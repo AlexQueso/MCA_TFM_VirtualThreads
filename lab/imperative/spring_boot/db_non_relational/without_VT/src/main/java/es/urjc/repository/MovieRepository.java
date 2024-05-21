9b86686781ddd989179074468557ce032a218465
Alex Quesada <a.quemen@gmail.com>
2024-05-21 22:32:07 +0200

    move files, delete unused projects

--- lab/imperative/spring_boot/db_non_relational/without_VT/src/main/java/es/urjc/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package es.urjc.repository;
-
-import es.urjc.entity.Movie;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface MovieRepository extends MongoRepository<Movie, String> {
-}
